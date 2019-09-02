da19e94069ef1c648cd1d87f81bc5001f3ba66a3
991rajat <991rajat@gmail.com>
2019-09-02 19:01:29 +0530

    Initial commit

---
+++ app/src/main/java/android/example/house_assist/MainActivity.java
@@ -0,0 +1,14 @@
+package android.example.house_assist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
