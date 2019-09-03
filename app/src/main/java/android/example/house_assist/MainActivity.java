471cbc414b437c561e77246a735e327201e130c3
991rajat <991rajat@gmail.com>
2019-09-03 20:01:04 +0530

    ADDED 1.Home Screen

--- app/src/main/java/android/example/house_assist/MainActivity.java
+++ app/src/main/java/android/example/house_assist/MainActivity.java
@@ -2,13 +2,27 @@ package android.example.house_assist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Checking If there is no current user logged in send it to Start Activity
+        if(FirebaseAuth.getInstance().getCurrentUser()==null)
+        {
+            startActivity(new Intent(MainActivity.this,Activity_Start.class));
+        }
     }
 }
