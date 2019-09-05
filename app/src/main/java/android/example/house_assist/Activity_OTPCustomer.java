15187ebaaef3f73cbd9e541c0e821cfe901b1c68
991rajat <991rajat@gmail.com>
2019-09-05 20:07:13 +0530

    ADDED 1. Dialog

--- app/src/main/java/android/example/house_assist/Activity_OTPCustomer.java
+++ app/src/main/java/android/example/house_assist/Activity_OTPCustomer.java
@@ -3,16 +3,20 @@ package android.example.house_assist;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.ybq.android.spinkit.style.FoldingCube;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
@@ -28,7 +32,8 @@ import java.util.concurrent.TimeUnit;
 public class Activity_OTPCustomer extends AppCompatActivity {
 
     private EditText otpCustomer;
-    private TextView otpCustomer_mobile;
+    private ProgressDialog progressDialog;
+    private TextView otpCustomer_mobile,otpcustomer_resend;
     private FirebaseAuth mAuth;
     private boolean mtoken = false;
     private Button otpSubmit;
@@ -47,13 +52,16 @@ public class Activity_OTPCustomer extends AppCompatActivity {
         password = intent.getStringExtra("password");
         email = intent.getStringExtra("email");
         Log.d(TAG,name+" "+mobile+" "+email+" "+password);
+
         //initialize
         otpCustomer = findViewById(R.id.otpcustomer);
         otpSubmit = findViewById(R.id.otpcustomer_submit);
         otpCustomer_mobile = findViewById(R.id.otpcustomer_mobile);
+        otpcustomer_resend = findViewById(R.id.otpcustomer_resend);
         mAuth = FirebaseAuth.getInstance();
         //Running Phone Authentication OTP
         otpCustomer_mobile.setText("+91"+mobile);
+        progressDialog = new ProgressDialog(this);
 
 
         otpSubmit.setOnClickListener(new View.OnClickListener() {
@@ -66,8 +74,9 @@ public class Activity_OTPCustomer extends AppCompatActivity {
                 }else
                 {
                     if(mtoken) {
-                        if(codeSent.equals(otpCustomer.getText().toString().trim()))
+                        if(codeSent.equals(otpCustomer.getText().toString().trim())) {
                             signInWithEmailAndPassword();
+                        }
                         else {
                             Toast.makeText(Activity_OTPCustomer.this, "OTP entered not valid.", Toast.LENGTH_LONG).show();
                         }
@@ -80,6 +89,18 @@ public class Activity_OTPCustomer extends AppCompatActivity {
             }
         });
 
+        otpcustomer_resend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.setCancelable(true);
+                progressDialog.setTitle("Hold ryt there sparky");
+                progressDialog.setMessage("OTP verification in Progress");
+                progressDialog.getWindow().setGravity(Gravity.CENTER);
+                progressDialog.show();
+                sendVerificationCode("+91"+mobile);
+            }
+        });
+
         mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
             public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
@@ -88,7 +109,7 @@ public class Activity_OTPCustomer extends AppCompatActivity {
 
             @Override
             public void onVerificationFailed(@NonNull FirebaseException e) {
-
+                progressDialog.dismiss();
             }
 
             @Override
@@ -100,6 +121,11 @@ public class Activity_OTPCustomer extends AppCompatActivity {
         };
         if(intent.getStringExtra("type").equals("OTP"))
         {
+            progressDialog.setCancelable(true);
+            progressDialog.setTitle("Hold ryt there sparky");
+            progressDialog.setMessage("OTP verification in Progress");
+            progressDialog.getWindow().setGravity(Gravity.CENTER);
+            progressDialog.show();
             sendVerificationCode("+91"+mobile);
         }
     }
@@ -125,11 +151,13 @@ public class Activity_OTPCustomer extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            progressDialog.dismiss();
                             startActivity(new Intent(Activity_OTPCustomer.this,MainActivity.class));
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(Activity_OTPCustomer.this, "Authentication failed.",
+                            progressDialog.dismiss();
+                            Toast.makeText(Activity_OTPCustomer.this, "Authentication failed. User Already Exist",
                                     Toast.LENGTH_SHORT).show();
 
                         }
@@ -139,6 +167,9 @@ public class Activity_OTPCustomer extends AppCompatActivity {
                 });
     }
 
+    @Override
+    public void onBackPressed() {
+    }
 
 
 }
