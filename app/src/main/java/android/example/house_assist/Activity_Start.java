40d804dd31a229778455ba5ba33efa355dffc2dd
991rajat <991rajat@gmail.com>
2019-09-05 13:36:16 +0530

    ADDED 1.Login 2.Registration with OTP

--- app/src/main/java/android/example/house_assist/Activity_Start.java
+++ app/src/main/java/android/example/house_assist/Activity_Start.java
@@ -2,13 +2,36 @@ package android.example.house_assist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity_Start extends AppCompatActivity {
+    private Button customer;
+    private Button service_Provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__start);
+        //Intilization
+        customer = findViewById(R.id.start_customer);
+        service_Provider = findViewById(R.id.start_service_provider);
+        //Onlick Events
+        customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Activity_Start.this,Activity_LogInCustomer.class));
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
     }
 }
--- app/src/main/java/android/example/house_assist/MainActivity.java
+++ app/src/main/java/android/example/house_assist/MainActivity.java
@@ -4,16 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button signOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signOut = findViewById(R.id.mainactivity_signout);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this,Activity_Start.class));
+            }
+        });
+
     }
 
     @Override
@@ -24,5 +36,6 @@ public class MainActivity extends AppCompatActivity {
         {
             startActivity(new Intent(MainActivity.this,Activity_Start.class));
         }
+
     }
 }
